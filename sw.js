ad6d0cbe40c92cdefd3614abb436512afd71fbb3
Vusi Mathonsi <118907642+byVusi@users.noreply.github.com>
2025-06-29 11:54:32 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * and serves cached assets when offline.
  */
 
-const CACHE_NAME = "Hercules-v2";
+const CACHE_NAME = "Hercules-v1";
 
 /**
  * List of static assets to cache during the install event.
